f8978258b44ae084ee1c939a1472ec5ea8f2d731
Anindita Bhattacharya <59307308+Anindita-Bhattacharya@users.noreply.github.com>
2022-01-13 13:01:58 +0530

    Create  Line to Cashier.py

---
+++ Line to Cashier.py
@@ -0,0 +1,13 @@
+n=int(input())
+num=[]
+x=list(map(int,input().split()))
+for i in range(n):
+    a=list(map(int,input().split()))
+    x1=0
+    for j in a:
+        x1=x1+(j*5)
+    x1=x1+(15*x[i])
+    num.append(x1)
+print(min(num))
+ 
+    
